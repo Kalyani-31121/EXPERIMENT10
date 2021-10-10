0ad0ac239cf4e8fc535b84c3016f20223a06d88b
KALYANI <kalyanikothekar19@gmail.com>
2021-10-10 23:14:14 +0530

    second commit

--- sample.py
+++ sample.py
@@ -1,1 +1,16 @@
-my name is kalyani
+kalyani
+a=input("Enter the first Number: ")
+b=input("Enter the second number: ")
+if int(a) > 0:
+    print('a is positive number')
+elif int(a) < 0:
+    print("a is negative number")
+else:
+     print("a is zero")
+     
+print("Numbers before swaping:")
+print('a='+a+"\n"+"b="+b)
+a,b=b,a
+print("Numbers after swapping: ")
+print('a='+a+"\n"+"b="+b)
+
